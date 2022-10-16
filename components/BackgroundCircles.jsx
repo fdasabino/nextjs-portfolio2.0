7916eeda97766840592ba7c96206fb4d6c0a94b0
Francisco Sabino <fdasabino@gmail.com>
2022-10-16 23:40:40 +0200

    update on bg animation

--- components/BackgroundCircles.jsx
+++ components/BackgroundCircles.jsx
@@ -18,16 +18,13 @@ export default function BackgroundCircles() {
       }}
       className="relative flex justify-center items-center"
     >
-      <div className="absolute border border-[#48837780] rounded-full h-[190px] w-[190px] mt-52 animate-ping" />
-      <div className="absolute border border-[#48837780] rounded-full md:h-[210px] md:w-[210px] mt-52 animate-pulse" />
-      <div className="absolute border border-[#48837780] rounded-full md:h-[240px] md:w-[240px] mt-52 animate-pulse" />
-      <div className="absolute border border-[#48837780] rounded-full md:h-[270px] md:w-[270px] mt-52 animate-pulse" />
-      <div className="absolute border border-[#48837744] rounded-full md:h-[300px] md:w-[300px] mt-52 animate-pulse" />
-      <div className="absolute border border-[#48837744] rounded-full md:h-[330px] md:w-[330px] mt-52 animate-pulse" />
-      <div className="absolute border border-[#48837744] rounded-full md:h-[360px] md:w-[360px] mt-52 animate-pulse" />
-      <div className="absolute border border-[#48837744] rounded-full md:h-[390px] md:w-[390px] mt-52 animate-pulse" />
-      <div className="absolute border border-[#4883775e] rounded-full md:h-[420px] md:w-[420px] mt-52 animate-pulse" />
-      <div className="absolute border border-[#48837744] rounded-full md:h-[450px] md:w-[450px] mt-52 animate-pulse" />
+      <div className="absolute border border-slate-800 rounded-full h-[150px] w-[150px] mt-52 animate-ping" />
+      <div className="absolute border border-teal-800 rounded-full h-[150px] w-[150px] mt-52 animate-ping" />
+      <div className="absolute border border-green-800 rounded-full h-[150px] w-[150px] mt-52 animate-ping" />
+      <div className="absolute border border-red-700 rounded-full h-[150px] w-[150px] mt-52 animate-ping" />
+      <div className="absolute border border-blue-700 rounded-full h-[150px] w-[150px] mt-52 animate-ping" />
+      <div className="absolute border border-orange-700 rounded-full h-[150px] w-[150px] mt-52 animate-ping" />
+      <div className="absolute border border-pink-700 rounded-full h-[150px] w-[150px] mt-52 animate-ping" />
     </motion.div>
   );
 }

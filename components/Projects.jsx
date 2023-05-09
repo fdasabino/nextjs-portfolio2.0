30f23e84cfa3de00882d6785b92c0571e6a6bcff
Francisco Sabino <fdasabino@gmail.com>
2023-05-09 12:47:47 +0200

    updated framer motion

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -13,6 +13,7 @@ const Projects = () => {
       <div className="sectionDivider" />
       <h3 className="sectionTitle">Projects</h3>
       <motion.div
+        className="w-full max-w-4xl"
         initial={{
           opacity: 0,
           scale: 0.5,
@@ -25,64 +26,60 @@ const Projects = () => {
           duration: 1.5,
         }}
       >
-        <div className="w-full max-w-4xl">
-          <Swiper
-            pagination={{
-              type: "progressbar",
-            }}
-            modules={[Pagination]}
-            className="mySwiper"
-          >
-            {ProjectsData.map((project, index) => (
-              <SwiperSlide key={project.id}>
-                <div className="mySwiper__container">
-                  <div className="mySwiper__wrapper">
-                    <div className="mySwiper__section">
-                      <p className="text-lg font-bold text-gray-400 md:text-xl">{project.title}</p>
-                      <div className="flex gap-4 my-4">
-                        {project.tags?.map((tag, index) => (
-                          <span key={index}>{tag}</span>
-                        ))}
-                      </div>
+        <Swiper
+          pagination={{
+            type: "progressbar",
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          {ProjectsData.map((project, index) => (
+            <SwiperSlide key={project.id}>
+              <div className="mySwiper__container">
+                <div className="mySwiper__wrapper">
+                  <div className="mySwiper__section">
+                    <p className="text-lg font-bold text-gray-400 md:text-xl">{project.title}</p>
+                    <div className="flex gap-4 my-4">
+                      {project.tags?.map((tag, index) => (
+                        <span key={index}>{tag}</span>
+                      ))}
                     </div>
-                    <div className="mySwiper__section">
-                      <div className="flex items-center flex-1 my-auto">
-                        <div className="flex flex-col gap-6 mx-10 text-justify md:mr-6 md:mx-0">
-                          <p className="text-sm text-gray-400 md:text-base">
-                            {project.description}
-                          </p>
-                          <div className="flex flex-row gap-4 text-sm text-center md:text-base xs:items-center xs:flex-col justify-evenly">
-                            {project.source !== "" && (
-                              <a
-                                className="mainButton"
-                                href={project.source}
-                                target="_blank"
-                                rel="noreferrer"
-                              >
-                                Source Code
-                              </a>
-                            )}
+                  </div>
+                  <div className="mySwiper__section">
+                    <div className="flex items-center flex-1 my-auto">
+                      <div className="flex flex-col gap-6 mx-10 text-justify md:mr-6 md:mx-0">
+                        <p className="text-sm text-gray-400 md:text-base">{project.description}</p>
+                        <div className="flex flex-row gap-4 text-sm text-center md:text-base xs:items-center xs:flex-col justify-evenly">
+                          {project.source !== "" && (
                             <a
                               className="mainButton"
-                              href={project.visit}
+                              href={project.source}
                               target="_blank"
                               rel="noreferrer"
                             >
-                              Live Project
+                              Source Code
                             </a>
-                          </div>
+                          )}
+                          <a
+                            className="mainButton"
+                            href={project.visit}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            Live Project
+                          </a>
                         </div>
                       </div>
-                      <div className="flex flex-1 max-w-sm my-4">
-                        <Image src={project.image} width={1000} height={1000} alt="" />
-                      </div>
+                    </div>
+                    <div className="flex flex-1 max-w-sm my-4">
+                      <Image src={project.image} width={1000} height={1000} alt="" />
                     </div>
                   </div>
                 </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </motion.div>
     </div>
   );

3661cbe3118bca52774c7c3baa5cdac58f983d22
Francisco Sabino <fdasabino@gmail.com>
2023-05-11 01:17:42 +0200

    updated about section styles

--- components/About.jsx
+++ components/About.jsx
@@ -9,68 +9,56 @@ const About = () => {
     <div className="relative flex flex-col items-center ">
       <div className="sectionDivider" />
       <h3 className="sectionTitle">About</h3>
-      <div className="flex flex-col items-center overflow-hidden text-center justify-evenly">
-        {AboutData.map((item) => (
-          <motion.div
-            initial={{
-              opacity: 0,
-              scale: 0.5,
-            }}
-            whileInView={{
-              opacity: 1,
-              scale: 1,
-            }}
-            transition={{
-              duration: 1.5,
-            }}
-            className="z-20 flex flex-col items-center justify-center max-w-5xl gap-5 mx-auto text-center md:text-left lg:flex-row"
-            key={item.id}
-          >
-            <motion.img
-              initial={{
-                opacity: 0,
-              }}
-              transition={{
-                duration: 1,
-              }}
-              whileInView={{
-                opacity: 1,
-              }}
-              className="object-cover border-2 border-teal-500 border-solid rounded-full w-36 h-36 md:w-56 md:h-56 lg:w-65 lg:h-65 lg:rounded-lg"
-              src={item.image}
-            />
-            <motion.div
-              className="p-3 shadow-2xl"
-              initial={{
-                opacity: 0,
-              }}
-              whileInView={{
-                opacity: 1,
-              }}
-              transition={{
-                duration: 1.5,
-              }}
-            >
-              <p className="aboutText">{item.text}</p>
-              <br />
-              <div>
-                <Timeline>
-                  {TimeLineData?.map((item, index) => (
-                    <Timeline.Item key={index} className="timelineText">
-                      <span>{item.year}</span> {item.text}
-                    </Timeline.Item>
-                  ))}
-                </Timeline>
-              </div>
-            </motion.div>
-          </motion.div>
-        ))}
-        <motion.div className="arrowDown">
-          <Link to="projects">
-            <BsChevronDoubleDown />
-          </Link>
-        </motion.div>
-      </div>
+      <motion.div
+        initial={{
+          opacity: 0,
+          scale: 0.5,
+        }}
+        whileInView={{
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className=""
+      >
+        <div className="mySwiper__container">
+          <div className="mySwiper__wrapper">
+            <div className="flex flex-col items-center overflow-hidden text-center justify-evenly">
+              {AboutData.map((item) => (
+                <div
+                  className="z-20 flex flex-col items-center justify-center max-w-5xl gap-5 mx-auto text-center md:text-left lg:flex-row"
+                  key={item.id}
+                >
+                  <motion.img
+                    className="object-cover border-2 border-teal-500 border-solid rounded-full w-36 h-36 md:w-56 md:h-56 lg:w-65 lg:h-65 lg:rounded-lg"
+                    src={item.image}
+                  />
+                  <div className="p-3 shadow-2xl">
+                    <p className="aboutText">{item.text}</p>
+                    <br />
+                    <div>
+                      <Timeline>
+                        {TimeLineData?.map((item, index) => (
+                          <Timeline.Item key={index} className="timelineText">
+                            <span>{item.year}</span> {item.text}
+                          </Timeline.Item>
+                        ))}
+                      </Timeline>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </motion.div>
+      <motion.div className="arrowDown">
+        <Link to="projects">
+          <BsChevronDoubleDown />
+        </Link>
+      </motion.div>
     </div>
   );
 };

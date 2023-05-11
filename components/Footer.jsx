234f1b1a0cecaf81f50cedf0b101a8658a07e3f8
Francisco Sabino <fdasabino@gmail.com>
2023-05-11 17:33:55 +0200

    updated copy link footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,22 +2,29 @@ import React from "react";
 import { Link } from "react-scroll/modules";
 import { Simplesharer } from "simple-sharer";
 import { Tooltip } from "antd";
-import { FaFacebookF, FaTwitter, FaWhatsapp, FaLinkedin, FaLink, FaRegEnvelope } from "react-icons/fa";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaWhatsapp,
+  FaLinkedin,
+  FaLink,
+  FaRegEnvelope,
+} from "react-icons/fa";
 
 const Footer = () => {
   const sh = new Simplesharer();
-  sh.url = "https://franciscosabino.com/"; //your url
+  sh.url = "https://franciscosabino.vercel.app/"; //your url
   sh.title = "Francisco Sabino - Portfolio"; //title for reddit, this is optional
   sh.text = "Front End Developer - Stockholm"; // description for twitter, not more than a hundred characters, optional.
   sh.hashtags = ["HTML", "CSS", "JavaScript", "React", "NextJs"]; // a list of hashtags for twitter,also optional
 
   return (
     <div className="p-5 bg-gray-600">
-      <div className="flex justify-evenly items-center">
-        <div className="flex gap-5 text-center flex-wrap">
+      <div className="flex items-center justify-evenly">
+        <div className="flex flex-wrap gap-5 text-center">
           {/* social icons */}
-          <div className="px-5 text-center mx-auto">
-            <p className="max-w-md text-center uppercase text-gray-800 text-sm md:text-md ">
+          <div className="px-5 mx-auto text-center">
+            <p className="max-w-md text-sm text-center text-gray-800 uppercase md:text-md ">
               Share my page on:
             </p>
             <div className="flex justify-center">
@@ -54,7 +61,7 @@ const Footer = () => {
           </div>
 
           {/* contact */}
-          <div className="flex justify-center items-center mx-auto">
+          <div className="flex items-center justify-center mx-auto">
             <div className="flex flex-col items-center px-5">
               <a className="footerLink" href="mailto:fdasabino@gmail.com">
                 <span className="text-lg">
@@ -66,7 +73,7 @@ const Footer = () => {
           </div>
 
           {/* links */}
-          <div className="w-full flex justify-center items-center gap-3">
+          <div className="flex items-center justify-center w-full gap-3">
             <div className="flex flex-col items-center">
               <Link className="footerLink" to="hero">
                 Home

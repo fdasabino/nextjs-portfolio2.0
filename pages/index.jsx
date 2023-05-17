7fce9f2e8bdfe8a522206f95833f2b36e474c1e2
Francisco Sabino <fdasabino@gmail.com>
2023-05-17 15:23:35 +0200

    removed landscape mode mobile

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,6 +13,7 @@ export default function Home() {
         <title>Francisco Sabino - Portfolio</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+        <meta http-equiv="ScreenOrientation" content="autoRotate:disabled" />
         <meta name="description" content="Front End Developer - Stockholm" />
         <meta name="keywords" content="HTML, CSS, JavaScript, React, NextJs" />
         <meta name="author" content="Francisco Sabino" />

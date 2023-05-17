b62607826b828ba452261d309d91773d1cd91c30
Francisco Sabino <fdasabino@gmail.com>
2023-05-17 15:00:58 +0200

    updated css

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,6 +12,7 @@ export default function Home() {
       <Head>
         <title>Francisco Sabino - Portfolio</title>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
         <meta name="description" content="Front End Developer - Stockholm" />
         <meta name="keywords" content="HTML, CSS, JavaScript, React, NextJs" />
         <meta name="author" content="Francisco Sabino" />

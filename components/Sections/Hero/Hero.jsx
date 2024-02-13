790857c8aeb200868d85433f769b44b95ef2d63b
Francisco Sabino <fdasabino@gmail.com>
2024-02-13 18:30:41 +0100

    Add Atropos library for image animation

--- components/Sections/Hero/Hero.jsx
+++ components/Sections/Hero/Hero.jsx
@@ -1,5 +1,7 @@
+import Atropos from "atropos/react";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import toast from "react-hot-toast";
 import { MdOutlineAlternateEmail } from "react-icons/md";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import { HeroData } from "../../../constants/constants";
@@ -27,34 +29,44 @@ const Hero = () => {
                 duration: 1.4,
                 delay: 0.6,
             }}>
-            {HeroData?.map((item) => (
-                <div
-                    key={item.id}
-                    className={styles.hero__wrapper}>
-                    <div className={styles.hero__image}>
-                        <Image
-                            priority
-                            height={600}
-                            width={600}
-                            key={item.id}
-                            src={item.image}
-                            alt={item.title}
-                        />
+            <Atropos
+                className={styles.hero__atropos}
+                rotateXMax={10}
+                rotateYMax={360}
+                activeOffset={40}
+                shadowOffset={40}
+                shadow={false}
+                highlight={false}
+                scaleChildren={false}>
+                {HeroData?.map((item) => (
+                    <div
+                        key={item.id}
+                        className={styles.hero__wrapper}>
+                        <div className={styles.hero__image}>
+                            <Image
+                                priority
+                                height={600}
+                                width={600}
+                                key={item.id}
+                                src={item.image}
+                                alt={item.title}
+                            />
+                        </div>
+                        <div className={styles.hero__text}>
+                            <h1>
+                                <span>{item.intro}</span> {item.title}
+                            </h1>
+                            <h2>
+                                <span>{text}</span>
+                                <Cursor cursorColor="#14a085" />
+                            </h2>
+                            <Button onClick={() => scrollTo("contact")}>
+                                Contact <MdOutlineAlternateEmail />
+                            </Button>
+                        </div>
                     </div>
-                    <div className={styles.hero__text}>
-                        <h1>
-                            <span>{item.intro}</span> {item.title}
-                        </h1>
-                        <h2>
-                            <span>{text}</span>
-                            <Cursor cursorColor="#14a085" />
-                        </h2>
-                        <Button onClick={() => scrollTo("contact")}>
-                            Contact <MdOutlineAlternateEmail />
-                        </Button>
-                    </div>
-                </div>
-            ))}
+                ))}
+            </Atropos>
         </motion.div>
     );
 };

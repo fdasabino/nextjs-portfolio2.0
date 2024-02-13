6fd791ff8b976f7e11e9e462e602f2ba4a73fd98
Francisco Sabino <fdasabino@gmail.com>
2024-02-13 20:01:34 +0100

    Update Hero component with useState and animation

--- components/Sections/Hero/Hero.jsx
+++ components/Sections/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import Atropos from "atropos/react";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import toast from "react-hot-toast";
+import { useState } from "react";
 import { MdOutlineAlternateEmail } from "react-icons/md";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import { HeroData } from "../../../constants/constants";
@@ -9,7 +9,13 @@ import { scrollTo } from "../../../utils/globalFunctions";
 import Button from "../../Layout/Button/Button";
 import styles from "./Hero.module.scss";
 
+const hiddenMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 30px, rgba(0,0,0,1) 30px, rgba(0,0,0,1) 30px)`;
+const visibleMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 30px)`;
+
 const Hero = () => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [isInView, setIsInView] = useState(false);
+
     const [text] = useTypewriter({
         words: ["<Front End Developer at Iquest/>", "<Student at Hyper Island/>"],
         loop: true,
@@ -39,7 +45,15 @@ const Hero = () => {
                 highlight={false}
                 scaleChildren={false}>
                 {HeroData?.map((item) => (
-                    <div
+                    <motion.div
+                        initial={false}
+                        animate={
+                            isLoaded && isInView
+                                ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
+                                : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
+                        }
+                        transition={{ duration: 1, delay: 1 }}
+                        whileInView={() => setIsInView(true)}
                         key={item.id}
                         className={styles.hero__wrapper}>
                         <div className={styles.hero__image}>
@@ -50,6 +64,7 @@ const Hero = () => {
                                 key={item.id}
                                 src={item.image}
                                 alt={item.title}
+                                onLoad={() => setIsLoaded(true)}
                             />
                         </div>
                         <div className={styles.hero__text}>
@@ -64,7 +79,7 @@ const Hero = () => {
                                 Contact <MdOutlineAlternateEmail />
                             </Button>
                         </div>
-                    </div>
+                    </motion.div>
                 ))}
             </Atropos>
         </motion.div>

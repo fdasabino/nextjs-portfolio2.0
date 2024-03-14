b2269b84e72a544cda2cece2417a330fcf145bb6
Francisco Sabino <fdasabino@gmail.com>
2024-03-14 13:13:14 +0100

    Update function to handle edge cases

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,57 @@
+import User from "@/models/User";
+import db from "@/utils/db";
+import bcrypt from "bcrypt";
+import { validateEmail } from "../../../utils/validateEmail";
+
+const handler = async (req, res) => {
+    try {
+        await db.connectDB();
+
+        const { name, email, password } = req.body;
+
+        if (!name || !email || !password) {
+            return res.status(422).json({ message: "Please fill in all fields..." });
+        }
+
+        if (!validateEmail(email)) {
+            return res.status(422).json({ message: "Invalid email address, please try again..." });
+        }
+
+        const user = await User.findOne({ email });
+        if (user && user.email === email) {
+            return res
+                .status(422)
+                .json({ message: "Email address not available, please try again..." });
+        }
+
+        if (password.length < 6) {
+            return res
+                .status(422)
+                .json({ message: "Password must be at least 6 characters long..." });
+        }
+
+        if (password.length > 20) {
+            return res
+                .status(422)
+                .json({ message: "Password must be less than 20 characters long..." });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        const createdUser = await newUser.save();
+        res.status(201).json({
+            message: `Sign up successful!`,
+            user: createdUser,
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export default handler;

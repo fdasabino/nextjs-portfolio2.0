e2cb211e80ee2218a05b711ee37583e1d87be2da
Francisco Sabino <fdasabino@gmail.com>
2024-03-15 15:53:52 +0100

    Update Loader styling and animation

--- components/Layout/Loader/Loader.tsx
+++ components/Layout/Loader/Loader.tsx
@@ -3,13 +3,8 @@ import styles from "./Loader.module.scss";
 const Loader = () => {
     return (
         <div className={styles.loader}>
+            <div className={styles.hypnotic} />
             <h2 className={styles.heading}>Loading...</h2>
-            <div className={styles.loading}>
-                <div className={styles.load} />
-                <div className={styles.load} />
-                <div className={styles.load} />
-                <div className={styles.load} />
-            </div>
         </div>
     );
 };

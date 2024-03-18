5a5ef8b3f21cd2a4a4e9205b92c2258aaaebb35d
Francisco Sabino <fdasabino@gmail.com>
2024-03-18 18:31:46 +0100

    Refactor rendering logic for Timeline, About, and Testimonials components

--- components/Home/Sections/About/About.tsx
+++ components/Home/Sections/About/About.tsx
@@ -7,18 +7,20 @@ const AboutComponent = ({ abouts }: any) => {
     return (
         <div className={styles.about}>
             <div className={`${styles.about__data} ${styles.base_blurry_background}`}>
-                {abouts.map((item: AboutProps) => (
-                    <div
-                        className={styles.about__data_item}
-                        key={item._id}>
-                        <div className={styles.about__data_img}>
-                            <motion.img src={item.image} />
-                        </div>
-                        <div className={styles.about__data_about}>
-                            <p>{item.description}</p>
-                        </div>
-                    </div>
-                ))}
+                {abouts
+                    ? abouts.map((item: AboutProps) => (
+                          <div
+                              className={styles.about__data_item}
+                              key={item._id}>
+                              <div className={styles.about__data_img}>
+                                  <motion.img src={item.image} />
+                              </div>
+                              <div className={styles.about__data_about}>
+                                  <p>{item.description}</p>
+                              </div>
+                          </div>
+                      ))
+                    : "No Abouts"}
             </div>
         </div>
     );
--- components/Home/Sections/Projects/Projects.tsx
+++ components/Home/Sections/Projects/Projects.tsx
@@ -35,49 +35,54 @@ const ProjectComponent = ({ projects }: any) => {
                     navigation={true}
                     modules={[Pagination, Navigation]}
                     className={styles.projects__swiper}>
-                    {projects.map((project: ProjectProps) => (
-                        <SwiperSlide key={project._id}>
-                            <div className={styles.projects__swiper_top}>
-                                <h2>{project.name}</h2>
-                                <div className={styles.projects__swiper_top_icons}>
-                                    {project.techTags?.map((icon, index) => {
-                                        return <span key={index}>{selectIcon(icon)}</span>;
-                                    })}
-                                </div>
-                            </div>
-                            <div className={styles.projects__swiper_bottom}>
-                                <div className={styles.projects__swiper_bottom_items}>
-                                    <div className={styles.projects__swiper_bottom__links}>
-                                        {project.repository !== "" && (
-                                            <a
-                                                href={project.repository}
-                                                target="_blank"
-                                                rel="noreferrer">
-                                                Source Code
-                                            </a>
-                                        )}
-                                        <a
-                                            href={project.live_url}
-                                            target="_blank"
-                                            rel="noreferrer">
-                                            Live Project
-                                        </a>
-                                    </div>
-                                    <div className={styles.projects__swiper_bottom__description}>
-                                        <p>{project.description}</p>
-                                    </div>
-                                </div>
-                                <div className={styles.projects__swiper_bottom_img}>
-                                    <Image
-                                        src={project.image}
-                                        width={600}
-                                        height={600}
-                                        alt={project.name}
-                                    />
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                    ))}
+                    {projects
+                        ? projects.map((project: ProjectProps) => (
+                              <SwiperSlide key={project._id}>
+                                  <div className={styles.projects__swiper_top}>
+                                      <h2>{project.name}</h2>
+                                      <div className={styles.projects__swiper_top_icons}>
+                                          {project.techTags?.map((icon, index) => {
+                                              return <span key={index}>{selectIcon(icon)}</span>;
+                                          })}
+                                      </div>
+                                  </div>
+                                  <div className={styles.projects__swiper_bottom}>
+                                      <div className={styles.projects__swiper_bottom_items}>
+                                          <div className={styles.projects__swiper_bottom__links}>
+                                              {project.repository !== "" && (
+                                                  <a
+                                                      href={project.repository}
+                                                      target="_blank"
+                                                      rel="noreferrer">
+                                                      Source Code
+                                                  </a>
+                                              )}
+                                              <a
+                                                  href={project.live_url}
+                                                  target="_blank"
+                                                  rel="noreferrer">
+                                                  Live Project
+                                              </a>
+                                          </div>
+                                          <div
+                                              className={
+                                                  styles.projects__swiper_bottom__description
+                                              }>
+                                              <p>{project.description}</p>
+                                          </div>
+                                      </div>
+                                      <div className={styles.projects__swiper_bottom_img}>
+                                          <Image
+                                              src={project.image}
+                                              width={600}
+                                              height={600}
+                                              alt={project.name}
+                                          />
+                                      </div>
+                                  </div>
+                              </SwiperSlide>
+                          ))
+                        : "No Projects"}
                 </Swiper>
             </div>
         </div>
